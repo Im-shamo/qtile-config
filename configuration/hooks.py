2a50613f28edb7dcdb9c586ce11b786499f1d42a
Im-shamo <samuelkwok2007@gmail.com>
2026-02-12 23:14:29 +0800

    Update hooks.py

--- configuration/hooks.py
+++ configuration/hooks.py
@@ -8,13 +8,3 @@ from configuration.environment_varables import set_environment_variables
 def startup_once():
     set_environment_variables()
     subprocess.Popen(scripts_dir / "startup.sh", start_new_session=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-
-# @hook.subscribe.client_managed
-# def floating_windows_front(client) -> None:
-#     """
-#     Floating windows spawn below others (exclusive to wayland)
-#     With this function we force floating windows to spawn front
-#     """
-#
-#     if client.floating:
-#         client.keep_above(enable=True)
